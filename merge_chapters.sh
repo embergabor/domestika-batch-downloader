d7affdc9169245410be5e65e708007babf4cc6a5
Gábor Ember <gabor.ember@finshape.com>
2023-05-30 21:59:57 +0200

    Merge script cleanup

--- merge_chapters.sh
+++ merge_chapters.sh
@@ -26,38 +26,16 @@ echo "Processing files in: $path"
   mkv_files=$(find "$path" -type f -name "*.mp4" -print0 | sort -z | tr '\0' ' ' | sed 's/ $//' | sed 's/ / + /g')
   mkv_files="${mkv_files# + }"  # Remove leading space and plus sign
 
-# Find all MKV files recursively in the input directory and add them to the array
-while IFS= read -r -d '' file; do
-    [[ -f $file ]] && file_list+=("$file")
-done < <(find "$path" -type f -name "*.mp4" -print0 | sort -z)
-
-# Check if any files were found
-if [[ ${#file_list[@]} -eq 0 ]]; then
-    echo "No MKV files found in the input directory."
-    exit 1
-fi
-
-  # Loop through the file list and extract a section of the filename as the chapter name
-  for file in "${mkv_files[@]}"; do
-      filename=$(basename "$file")
-      chapter_name=${filename#*-}  # Extract from the first "-" until the end
-      chapter_name=${chapter_name//-/ }  # Replace "-" with spaces
-      chapter_names+=("$chapter_name")
-  done
-
-  # Join the chapter names into a single string for the chapter names argument
-  chapter_names_arg=$(IFS=":"; echo "${chapter_names[*]}")
-
   mkvmerge -o "$path/$folder_name.mkv" --generate-chapters when-appending --generate-chapters-name-template "<FILE_NAME>" $mkv_files
 
-  echo "Merged MKV file: $path/$folder_name.mkv"
-
-  chapter_file="chapter.xml"
+  chapter_file="$path/chapter.xml"
   mkvextract "$path/$folder_name.mkv" chapters "$chapter_file"
 
-  # Replace "-" with spaces in the chapter XML
+  # Get the lesson title as chapter name and replace "-" with spaces
   sed -i '' -E 's/(<ChapterString>).{5}(.*)(<\/ChapterString>)/\1\2\3/' "$chapter_file"
   sed -i '' -E '/<ChapterString>/,/<\/ChapterString>/ s/-/ /g' "$chapter_file"
 
   # Update the modified chapter XML using mkvpropedit
   mkvpropedit "$path/$folder_name.mkv" --chapters "$chapter_file"
+
+  echo "Merged MKV file: $path/$folder_name.mkv"
