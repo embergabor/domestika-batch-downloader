e1de8c1cff99b543de3e3661ce5eb64fd74d66db
Gábor Ember <gabor.ember@finshape.com>
2023-05-30 09:38:05 +0200

    Batch download courses from input.txt

--- index.js
+++ index.js
@@ -4,34 +4,63 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const m3u8ToMp4 = require('m3u8-to-mp4');
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 const converter = new m3u8ToMp4();
+const fetch = require("node-fetch");
 
 const debug = false;
 const debug_data = [];
 
-const course_url = '';
+const session= '5jPt%2FfugtvjPfAyucfu06rK4ltwptKwsgiZBi3%2F%2FryM5xfKA4spU9NXTH78jWfV2CrlvC3%2BVK4Srm2ABQn70bbAjOnCyPsn2Hiyk%2BLNq%2B7sM9QYQMqOSPI6BhepQKVctaZOjbJYFjnnQSJl0zIUXXJyut%2FrPMyh%2FxNVPF3PF0Y5tyNvIg4Gmoiyv6KJZv65aSOGivfNgEIucosDVOG%2FqSdbq7sqxqzp3GUAgcRwXTrVY1YvbNpAbfFp1z%2BFiOefBmSd%2B7VgiUVTjXKkqH9ET2fClpCjqAbrlXMog%2F6lhaMNZU2NryEEfd46p%2BuJKM3EN0ZLmY5V%2FqbDERhojr61Xvl6ofRB9OAOUp1zV4k7C5Vz7AkbbUesnDEnBqvFBTg94gxgqEjylWQnwpT1kFZMPUvKH0%2BVDohGfMQoY1W4NW7QtMJp5amb1kd6mTeHqBih%2FwUKOrbysAUITxoT1%2FlQwbjbiZ0pFXbAAGJ6BbQthRaOwLJlL0N83TZg%2FhmchOo5FZwIHg27ZpsAHLTyC4jmqMLgCaG5FnlXTV7kvIgGUCK8z3WSPRUXDXTlWZFhDkCRthAw7Uv4DuqoxM%2BRxNTsq%2FokLNlq6TPmO7bg1flSiirb1bTEl6HdGTNfGZttafnpClWtMtuLwB5siOca6QQZ3vvrxtLTz3IaYept2MJvWINkRYKXAkn9479nUg5hKP%2FYSjuT5PTPGmPoFuSLq2Ktkk1V5WKQFmw%3D%3D--Cb7abO2sX%2FYvsktt--Aoe1PXnLF%2BWOL6BjXa8MXg%3D%3D';
+
+
+//Credentials needed for the access token to get the final project
+const _credentials_ = '{%22accessToken%22:%22pOEcPRTn98yDyT9hOc5jSnHgslssDF8iJr-iL5xR3pQ%22%2C%22refreshToken%22:%22c8G-e1z__PHa7bsC3CENKxzOKRAiXiZjBmY-z7VeOpc%22%2C%22isEmpty%22:false}';
+
+
 const subtitle_lang = 'en';
 
 //Cookie used to retreive video information
 const cookies = [
     {
         name: '_domestika_session',
-        value: '',
+        value: session,
         domain: 'www.domestika.org',
     },
 ];
 
-//Credentials needed for the access token to get the final project
-const _credentials_ = '';
 
 //Check if the N_m3u8DL-RE.exe exists, throw error if not
-if (fs.existsSync('N_m3u8DL-RE.exe')) {
-    scrapeSite();
+if (fs.existsSync('N_m3u8DL-RE')) {
+    downloadCourses();
 } else {
     throw Error('N_m3u8DL-RE.exe not found! Download the Binary here: https://github.com/nilaoda/N_m3u8DL-RE/releases');
 }
 
-async function scrapeSite() {
+async function asyncReadFile(filename) {
+    try {
+        const contents = await fsPromises.readFile(filename, 'utf-8');
+
+        const arr = contents.split(/\r?\n/);
+
+        //console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
+        console.log(arr.length + " course URLs found");
+
+        return arr;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function downloadCourses() {
+    const course_urls = await asyncReadFile("input.txt");
+    for(const course of course_urls) {
+        console.log(course);
+        await scrapeSite(course);
+    }
+}
+
+async function scrapeSite(course_url) {
     //Scrape site for links to videos
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
@@ -47,7 +76,7 @@ async function scrapeSite() {
     let title = $('h1.course-header-new__title')
         .text()
         .trim()
-        .replace(/[/\\?%*:|"<>]/g, '-');
+        .replace(/[/\\?%*:'|"<>]/g, '').replaceAll(" ","_");
 
     let totalVideos = 1;
 
@@ -58,7 +87,7 @@ async function scrapeSite() {
             title: $(units[i])
                 .text()
                 .trim()
-                .replace(/[/\\?%*:|"<>]/g, '-'),
+                .replace(/[/\\?%*:'|"<>]/g, '').replaceAll(" ","_"),
             videoData: videoData,
         });
 
@@ -77,11 +106,13 @@ async function scrapeSite() {
     final_data = await fetchFromApi(`https://api.domestika.org/api/videos/${final_project_id}?with_server_timing=true`, 'video.v1', access_token);
 
     allVideos.push({
-        title: 'Final project',
+        title: 'U9-Final_project',
         videoData: [{ playbackURL: final_data.data.attributes.playbackUrl, title: 'Final project' }],
     });
 
     //Loop through all files and download them
+
+    console.log("Title: " + title);
     let count = 0;
     for (let i = 0; i < allVideos.length; i++) {
         const unit = allVideos[i];
@@ -92,8 +123,24 @@ async function scrapeSite() {
                 fs.mkdirSync(`domestika_courses/${title}/${unit.title}/`, { recursive: true });
             }
 
-            let log = await exec(`N_m3u8DL-RE -sv res="1080*":codec=hvc1:for=best "${vData.playbackURL}" --save-dir "domestika_courses/${title}/${unit.title}" --save-name "${a}_${vData.title}"`);
-            let log2 = await exec(`N_m3u8DL-RE --auto-subtitle-fix --sub-format SRT --select-subtitle lang="${subtitle_lang}":for=all "${vData.playbackURL}" --save-dir "domestika_courses/${title}/${unit.title}" --save-name "${a}_${vData.title}"`);
+            const unitNumber = unit.title.slice(0, 2).replace("U","S");
+            const filename = unitNumber + "E" + (a+1) + "-" + unit.title.slice(3).trim().replace(/[/\\?%*:|"<>]/g, '-').replaceAll(" ", "_") + "-" + vData.title.trim().replace(/[/\\?%*:|"<>]/g, '-').replaceAll(" ", "_");
+            console.log(filename);
+            console.log("domestika_courses/" + title + "/" + filename + ".mp4");
+            if (!fs.existsSync("domestika_courses/" + title + "/" + filename + ".mp4")) {
+                let log = await exec(`./N_m3u8DL-RE -sv res="1080*":codec=hvc1:for=best "${vData.playbackURL}" --save-dir "domestika_courses/${title}" --save-name "${filename}"`);
+                let log2 = await exec(`./N_m3u8DL-RE --auto-subtitle-fix --sub-format SRT --select-subtitle lang="${subtitle_lang}":for=all "${vData.playbackURL}" --save-dir "domestika_courses/${title}" --save-name "${filename}"`);
+            } else {
+                console.log("Already downloaded");
+            }
+            if (!fs.existsSync("domestika_courses/" + title + "/" + filename + ".remux.mp4")) {
+                if (fs.existsSync("domestika_courses/" + title + "/" + filename + ".mp4") && fs.existsSync("domestika_courses/" + title + "/" + filename + ".en.srt")) {
+                    let log3 = exec(`ffmpeg -i "domestika_courses/${title}/${filename}.mp4" -i "domestika_courses/${title}/${filename}.en.srt" -c copy -c:s mov_text "domestika_courses/${title}/${filename}.remux.mp4"`);
+                } else if (fs.existsSync("domestika_courses/" + title + "/" + filename + ".mp4")) {
+                    await exec(`cp "domestika_courses/${title}/${filename}.mp4" "domestika_courses/${title}/${filename}.remux.mp4"`);
+                }
+            }
+
 
             if (debug) {
                 debug_data.push({
@@ -105,14 +152,28 @@ async function scrapeSite() {
             count++;
             console.log(`Download ${count}/${totalVideos} Downloaded`);
         }
+
     }
 
     await browser.close();
+
+    if (fs.existsSync("fl.txt")){
+        await exec(`rm "fl.txt"`);
+    }
+
+    if (!fs.existsSync("domestika_courses/" + title + ".mp4")){
+        console.log("Concat videos")
+        await exec(`find "domestika_courses/${title}" -name "*.remux.mp4" | sort | sed 's:\ :\\\ :g'| sed 's/^/file /' > fl.txt;`);
+        let log4 = await exec(`ffmpeg -f concat -safe 0 -i fl.txt -c copy -scodec copy "domestika_courses/${title}.mp4"`);
+        await exec(`rm "fl.txt"`);
+        console.log(log4);
+    }
 
     if (debug) {
         fs.writeFileSync('log.json', JSON.stringify(debug_data));
         console.log('Log File Saved');
     }
+
 
     console.log('All Videos Downloaded');
 }
